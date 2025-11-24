3d58f90d49b2093c25c5963ff1416fc75ef4484e
Salijashahi <12301167@students.koi.edu.au>
2025-11-24 15:15:56 +1100

    tailwind fixed, login and signup page working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,11 @@ import ClubJoinMainPage from "./Pages/Club/ClubJoinMainPage";
 import ClubRedirectPage from "./Pages/Club/ClubRedirectPage";
 import CoachesPage from "./Pages/Coaches/CoachesPage";
 
+
+import LoginPage from "./Pages/Contactuspage/login";
+import SignupPage from "./Pages/Contactuspage/signup";
+import DashboardPage from "./Pages/Contactuspage/dashboard/";
+
 export default function App() {
   return (
     <Router>
@@ -17,6 +22,13 @@ export default function App() {
         <Route path="/clubs/join" element={<ClubJoinMainPage />} />
         <Route path="/clubs/redirect" element={<ClubRedirectPage />} />
         <Route path="/coaches" element={<CoachesPage />} />
+
+        {/* Auth Pages */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/* Protected Dashboard */}
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
